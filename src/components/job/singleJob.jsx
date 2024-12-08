528583c860b160d6b7c461b813879feea1369c93
Nurul Insani Dewi <nurulinsanid@gmail.com>
2024-12-08 08:10:34 +0700

    add apply button

--- src/components/job/singleJob.jsx
+++ src/components/job/singleJob.jsx
@@ -1,35 +1,56 @@
-"use client";
+'use client';
 
-import { Building2 } from "lucide-react";
-import { usePathname } from "next/navigation";
+import { ArrowUpRightFromSquare, Building2 } from 'lucide-react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export const SingleJob = ({ data }) => {
   const currentPath = usePathname();
-  const pathSlug = currentPath.split("/").pop();
+  const pathSlug = currentPath.split('/').pop();
 
   const filterJob = (data) => {
     return data.data.filter((job) => job.slug == pathSlug);
   };
-
   const jobs = filterJob(data);
-  // console.log(jobs)
+  // console.log(jobs);
 
   return (
     <>
       {jobs.map((job, i) => (
-        <div key={i} className="p-3 bg-white rounded-3xl border-[0.5px] border-black">
-          <div className="p-7 bg-gray-100 rounded-3xl">
-            <h1 className="text-4xl font-semibold font-mono mb-4 leading-[1.2] break-words">{job.title}</h1>
+        <div
+          key={i}
+          className="p-3 bg-white rounded-3xl border-[0.5px] border-black"
+        >
+          <div
+            className={[
+              'p-7 rounded-3xl',
+              job.remote ? 'bg-primary/50' : 'bg-violet-100',
+            ].join(' ')}
+          >
+            <h1 className="text-4xl font-semibold font-mono mb-4 leading-[1.2] break-words">
+              {job.title}
+            </h1>
             <h3 className="text-xl text-gray-500 capitalize">
-              <Building2 className="inline-block -translate-y-[3px] mr-2"/>
+              <Building2 className="inline-block -translate-y-[3px] mr-2" />
               {job.company_name}
-              {job.remote && (<span className="font-semibold inline-block text-sm text-black ml-4 -translate-y-[1px]">Remote</span>)}
+              {job.remote && (
+                <span className="font-semibold inline-block text-sm text-black ml-4 -translate-y-[1px]">
+                  Remote
+                </span>
+              )}
             </h3>
           </div>
           <div
             dangerouslySetInnerHTML={{ __html: job.description }}
             className="content space-y-4 py-5 px-7"
           />
+          <Link
+            href={job.url}
+            className="flex gap-2 items-center justify-center w-full text-center p-4 border bg-violet-500 rounded-2xl text-white"
+            target="_blank"
+          >
+            Apply <ArrowUpRightFromSquare size={16} />
+          </Link>
         </div>
       ))}
     </>
